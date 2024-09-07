cdab8f55c35b288380b868c29c32a1d7bd829b05
seanhyper <123296553+seanhyper@users.noreply.github.com>
2024-09-07 13:37:16 +0300

    add login/register page

---
+++ frontend/src/Dashboard.js
@@ -0,0 +1,13 @@
+// src/Dashboard.js
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Welcome to the Dashboard</h2>
+      {/* Add more dashboard content here */}
+    </div>
+  );
+}
+
+export default Dashboard;
